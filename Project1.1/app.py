7cb1bc1e2b8b82a4b13ced75d43f26f1d4dc8337
audihangkulung <60369629+audihangkulung@users.noreply.github.com>
2021-07-31 18:03:15 -0500

    intial code push

---
+++ Project1.1/app.py
@@ -0,0 +1,11 @@
+pound = input("Please enter your weight in pounds: ")
+kg = int(pound)*0.453592
+
+print("Your weight in kg is: " + str(kg))
+
+a = input("Please enter a number: ")
+b = input("Please enter another number: ")
+
+sum = int(a) + int(b)
+print('The sum of given numbers ' + a + " & " + b + " is " + str(sum))
+
